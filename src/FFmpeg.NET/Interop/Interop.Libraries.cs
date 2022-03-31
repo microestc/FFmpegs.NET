9a2f2e34651b3d8b73f2f2e70d33127137a8ffac
microestc <microestc@outlook.com>
2022-03-31 18:09:24 +0800

    2022-3-31

--- src/FFmpeg.NET/Interop/Interop.Libraries.cs
+++ src/FFmpeg.NET/Interop/Interop.Libraries.cs
@@ -1,82 +1,156 @@
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace FFmpeg.NET.Interop
 {
     internal sealed class Libraries
     {
-        public const string Libdl = "libdl";
+        public const string AV_UTIL = "avutil";
+        public const string SW_SCALE = "swscale";
+        public const string POSTPROC = "postproc";
+        public const string SW_RESAMPLE = "swresample";
+        public const string AV_CODEC = "avcodec";
+        public const string AV_FORMAT = "avformat";
+        public const string AV_FILTER = "avfilter";
+        public const string AV_DEVICE = "avdevice";
 
-        public const string Libavcodec = "avcodec";
+        internal const string DLLS_DIR = "lib";
 
-        public const string Libavdevice = "avdevice";
-
-        public const string Libavfilter = "avfilter";
-
-        public const string Libavformat = "avformat";
-
-        public const string Libavutil = "avutil";
-
-        public const string Libpostproc = "postproc";
-
-        public const string Libswscale = "swscale";
-
-        public const string Libswresample = "swresample";
-
-        public static Func<string, string> OSPlatformLibrary { get; set; }
-
-        public static readonly IDictionary<string, string[]> Dependencies = new Dictionary<string, string[]>(StringComparer.CurrentCulture)
+        // 动态库依赖关系
+        public static readonly IDictionary<string, string[]> LibraryDependencies = new Dictionary<string, string[]>(StringComparer.CurrentCulture)
         {
-            { Libavutil, new string[]{} },
-            { Libavfilter, new []{ Libavutil } },
-            { Libswscale, new []{ Libavutil } },
-            { Libpostproc, new []{ Libavutil } },
-            { Libavcodec, new []{ Libavutil } },
-            { Libswresample, new []{ Libavutil } },
-            { Libavformat, new []{ Libavutil, Libavcodec } },
-            { Libavdevice, new []{ Libavutil, Libavcodec, Libavformat } },
+            { AV_UTIL, new string[]{} },
+            { SW_SCALE, new []{ AV_UTIL } },
+            { POSTPROC, new []{ AV_UTIL } },
+            { SW_RESAMPLE, new []{ AV_UTIL } },
+            { AV_CODEC, new []{ AV_UTIL, SW_RESAMPLE } },
+            { AV_FORMAT, new []{ AV_UTIL, SW_RESAMPLE, AV_CODEC } },
+            { AV_FILTER, new []{ AV_UTIL, SW_SCALE, POSTPROC, SW_RESAMPLE, AV_CODEC, AV_FORMAT } },
+            { AV_DEVICE, new []{ AV_UTIL, SW_SCALE, POSTPROC, SW_RESAMPLE, AV_CODEC, AV_FORMAT, AV_FILTER } },
         };
 
-        public static readonly IDictionary<string, int> LIBRARY_VERSIONS = new Dictionary<string, int>(StringComparer.CurrentCulture)
+        public static readonly IDictionary<string, int> LIBRARYVERSION_MAPS = new Dictionary<string, int>(StringComparer.CurrentCulture)
         {
-            { Libavutil, 0 },
-            { Libavfilter, 0 },
-            { Libswscale, 0 },
-            { Libpostproc, 0 },
-            { Libavcodec, 0 },
-            { Libswresample, 0 },
-            { Libavformat, 0 },
-            { Libavdevice, 0 },
+            { AV_UTIL, 57 },
+            { SW_SCALE, 6 },
+            { POSTPROC, 56 },
+            { SW_RESAMPLE, 4 },
+            { AV_CODEC, 59 },
+            { AV_FORMAT, 59 },
+            { AV_FILTER, 8 },
+            { AV_DEVICE, 59 },
         };
 
         public static Dictionary<string, IntPtr> LoadedLibraries = new Dictionary<string, IntPtr>(StringComparer.CurrentCulture);
 
-        static Libraries()
+        /// <summary>
+        /// OS PlatformID
+        /// </summary>
+        public static PlatformID OSPlatformID = OperatingSystem.IsWindows() ? PlatformID.Win32NT : OperatingSystem.IsLinux() ? PlatformID.Unix : OperatingSystem.IsMacOS() ? PlatformID.MacOSX : PlatformID.Other;
+
+        /// <summary>
+        /// OS 架构 (linux-x64,linux-x86,linux-arm64,linux-arm,win-x64,win-x86,win-arm64,win-arm,macos)
+        /// </summary>
+        public static string OsPlatform = (OperatingSystem.IsWindows() ? "win" : OperatingSystem.IsLinux() ? "linux" : OperatingSystem.IsMacOS() ? "macos" : "other") + RuntimeInformation.OSArchitecture switch
         {
-            OSPlatformLibrary = GetOsPlatformLibraryName;
-            foreach (var row in Dependencies)
+            Architecture.X64 => "-x64",
+            Architecture.X86 => "-x86",
+            Architecture.Arm64 => "-arm64",
+            Architecture.Arm => "-arm",
+            _ => string.Empty
+        };
+
+
+        public Libraries()
+        {
+            foreach (var row in LIBRARYVERSION_MAPS)
             {
-                var library = OSPlatformLibrary(row.Key);
-                var libraryDependencies = row.Value;
-                if (libraryDependencies.Any(n => !LoadedLibraries.ContainsKey(n)))
+                IntPtr handle;
+                // frist load external libraries. 
+                if (NativeLibrary.TryLoad(PlatformLibraryPath(row.Key, row.Value), typeof(Libraries).Assembly, DllImportSearchPath.UseDllDirectoryForDependencies, out handle))
                 {
-                    foreach (var item in libraryDependencies.Where(n => !LoadedLibraries.ContainsKey(n)))
+                    LoadedLibraries.Add(row.Key, handle);
+                    continue;
+                }
+                if (NativeLibrary.TryLoad(PlatformLibraryPath(row.Key), typeof(Libraries).Assembly, DllImportSearchPath.UseDllDirectoryForDependencies, out handle))
+                {
+                    LoadedLibraries.Add(row.Key, handle);
+                    continue;
+                }
+                // load internal libraries
+                if (NativeLibrary.TryLoad(PlatformLibraryPath(row.Key, row.Value, true), typeof(Libraries).Assembly, DllImportSearchPath.AssemblyDirectory, out handle))
+                {
+                    LoadedLibraries.Add(row.Key, handle);
+                    continue;
+                }
+                if (NativeLibrary.TryLoad(PlatformLibraryPath(row.Key, @internal: true), typeof(Libraries).Assembly, DllImportSearchPath.AssemblyDirectory, out handle))
+                {
+                    LoadedLibraries.Add(row.Key, handle);
+                    continue;
+                }
+                throw new DllNotFoundException($"Not found dynamic library '{row.Key}'.");
+            }
+        }
+
+
+        /// <summary>
+        /// 通过动态库名称构建不同平台下可加载动态库路径
+        /// </summary>
+        /// <param name="libraryName">动态库名称</param>
+        /// <param name="version">动态库后缀版本号</param>
+        /// <param name="internal">是否使用内部动态库</param>
+        /// <returns></returns>
+        public static string PlatformLibraryPath(string libraryName, int? version = null, bool @internal = false)
+        {
+            if (string.IsNullOrEmpty(libraryName)) throw new ArgumentNullException(nameof(libraryName));
+            if (@internal)
+            {
+                if (version.HasValue)
+                {
+                    return OSPlatformID switch
                     {
-                        if (!NativeLibrary.TryLoad(OSPlatformLibrary(item), typeof(Libraries).Assembly, DllImportSearchPath.AssemblyDirectory, out IntPtr handle))
-                            throw new DllNotFoundException();
-                    }
+                        PlatformID.Win32NT => Path.Combine(DLLS_DIR, OsPlatform, $"{libraryName}-{version}.dll"),
+                        PlatformID.Unix => Path.Combine(DLLS_DIR, OsPlatform, $"lib{libraryName}.so.{version}"),
+                        PlatformID.MacOSX => Path.Combine(DLLS_DIR, OsPlatform, $"lib{libraryName}.{version}.dylib"),
+                        _ => throw new PlatformNotSupportedException()
+                    };
+                }
+                else
+                {
+                    return OSPlatformID switch
+                    {
+                        PlatformID.Win32NT => Path.Combine(DLLS_DIR, OsPlatform, $"{libraryName}.dll"),
+                        PlatformID.Unix => Path.Combine(DLLS_DIR, OsPlatform, $"lib{libraryName}.so"),
+                        PlatformID.MacOSX => Path.Combine(DLLS_DIR, OsPlatform, $"lib{libraryName}.dylib"),
+                        _ => throw new PlatformNotSupportedException()
+                    };
+                }
+            }
+            else
+            {
+                if (version.HasValue)
+                {
+                    return OSPlatformID switch
+                    {
+                        PlatformID.Win32NT => $"{libraryName}-{version}.dll",
+                        PlatformID.Unix => $"lib{libraryName}.so.{version}",
+                        PlatformID.MacOSX => $"lib{libraryName}.{version}.dylib",
+                        _ => throw new PlatformNotSupportedException()
+                    };
+                }
+                else
+                {
+                    return OSPlatformID switch
+                    {
+                        PlatformID.Win32NT => $"{libraryName}.dll",
+                        PlatformID.Unix => $"lib{libraryName}.so",
+                        PlatformID.MacOSX => $"lib{libraryName}.dylib",
+                        _ => throw new PlatformNotSupportedException()
+                    };
                 }
             }
         }
 
-        public static string GetOsPlatformLibraryName(string libraryName)
-        {
-            if (!LIBRARY_VERSIONS.TryGetValue(libraryName, out var version)) throw new KeyNotFoundException($"Dynamic libraries Verions Map not found any key '{libraryName}'.");
-            if (OperatingSystem.IsWindows()) return $"{libraryName}-{version}.dll";
-            if (OperatingSystem.IsLinux()) return $"lib{libraryName}.so.{version}";
-            if (OperatingSystem.IsMacOS()) return $"lib{libraryName}.{version}.dylib";
-            throw new PlatformNotSupportedException();
-        }
+
 
     }
 }
